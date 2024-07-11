f2318c5f6668fc6fbba3bda0cb6c499c97d5b216
k3llucky <kjrobinson@csuchico.edu>
2024-07-11 20:15:13 +0000

    made changes to java script

--- .js
+++ .js
@@ -1,178 +1,184 @@
 function setup(){
-    createCanvas (1000, 1000);
+    createCanvas (800, 800);
 
 }
 
 
  function draw() {
-    
-    background(20);
-    rotate(ellipse)
+    fill(0, 0, 0);
+    background(300);
+
 
     for (let i=0; i<6; i++) {
         resetMatrix(); 
         let angle = (TWO_PI/6)*i;
         translate(
             sin(angle)*1,
-            cos(angle)*1,
+            cos(angle)*1
         );
-        ellipse(500, 500, 2);
+        ellipse(400, 400, 0);
     }
-    
 
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*550,
-                cos(angle)*550,
-            );
-            ellipse(500, 500, 80);
-        }
+    for (let i=0; i<9; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/9)*i;
+        translate(
+            sin(angle)*5,
+            cos(angle)*5
+        );
+        ellipse(400, 400, 1);
+    }
 
-        for (let i=0; i<10; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/8)*i;
-            translate(
-                sin(angle)*550,
-                cos(angle)*550,
-            );
-            
-            ellipse(500, 500, 79);
-        }
+    for (let i=0; i<13; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/13)*i;
+        translate(
+            sin(angle)*10,
+            cos(angle)*10
+        );
+        ellipse(400, 400, 2);
+    }
 
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*40,
-                cos(angle)*40,
-            );
-            
-            ellipse(500, 500, 13);
-        }
+     for (let i=0; i<16; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/16)*i;
+        translate(
+            sin(angle)*16,
+            cos(angle)*16
+        );
+        ellipse(400, 400, 3);
+    }
 
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*50,
-                cos(angle)*50,
-            );
-            
-            ellipse(500, 500, 14);
-        }
+    for (let i=0; i<19; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/19)*i;
+        translate(
+            sin(angle)*23,
+            cos(angle)*23
+        );
+        ellipse(400, 400, 4);
+    }
 
+    for (let i=0; i<22; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/22)*i;
+        translate(
+            sin(angle)*31,
+            cos(angle)*31
+        );
+        ellipse(400, 400, 5);
+    }
+    for (let i=0; i<25; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/25)*i;
+        translate(
+            sin(angle)*40,
+            cos(angle)*40
+        );
+        ellipse(400, 400, 6);
+    }
 
-for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*60,
-                cos(angle)*60,
-            );
-            
-            ellipse(500, 500, 15);
-        }
-
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*70,
-                cos(angle)*70,
-            );
-            
-            ellipse(500, 500, 16);
-        }
-
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*80,
-                cos(angle)*80,
-            );
-            
-            ellipse(500, 500, 17);
-        }
-
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*90,
-                cos(angle)*90,
-            );
-            
-            ellipse(500, 500, 18);
-        }
-
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*100,
-                cos(angle)*100,
-            );
-            
-            ellipse(500, 500, 19);
-        }
-
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*110,
-                cos(angle)*110,
-            );
-            
-            ellipse(500, 500, 20);
-        }
+    for (let i=0; i<28; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/28)*i;
+        translate(
+            sin(angle)*50,
+            cos(angle)*50
+        );
+        ellipse(400, 400, 7);
+    }
 
 
-       for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*120,
-                cos(angle)*120,
-            );
-            
-            ellipse(500, 500, 21);
-        }
+ for (let i=0; i<31; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/31)*i;
+        translate(
+            sin(angle)*61,
+            cos(angle)*61
+        );
+        ellipse(400, 400, 8);
+    }
 
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*130,
-                cos(angle)*130,
-            );
-            
-            ellipse(500, 500, 22);
-        }
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*140,
-                cos(angle)*140,
-            );
-            
-            ellipse(500, 500, 23);
-        }
+    for (let i=0; i<34; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/34)*i;
+        translate(
+            sin(angle)*73,
+            cos(angle)*73
+        );
+        ellipse(400, 400, 9);
+    }
 
-        for (let i=0; i<6; i++) {
-            resetMatrix(); 
-            let angle = (TWO_PI/6)*i;
-            translate(
-                sin(angle)*150,
-                cos(angle)*150,
-            );
-            
-            ellipse(500, 500, 24);
-        }
+    for (let i=0; i<37; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/37)*i;
+        translate(
+            sin(angle)*86,
+            cos(angle)*86
+        );
+        ellipse(400, 400, 10);
+    }
+
+    for (let i=0; i<40; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/40)*i;
+        translate(
+            sin(angle)*100,
+            cos(angle)*100
+        );
+        ellipse(400, 400, 11);
+    }
+
+    for (let i=0; i<43; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/43)*i;
+        translate(
+            sin(angle)*115,
+            cos(angle)*115
+        );
+        ellipse(400, 400, 12);
+    }
+
+    for (let i=0; i<46; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/46)*i;
+        translate(
+            sin(angle)*131,
+            cos(angle)*131
+        );
+        ellipse(400, 400, 13);
+    }
+
+    for (let i=0; i<49; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/49)*i;
+        translate(
+            sin(angle)*148,
+            cos(angle)*148
+        );
+        ellipse(400, 400, 14);
+    }
+
+    for (let i=0; i<52; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/52)*i;
+        translate(
+            sin(angle)*166,
+            cos(angle)*166
+        );
+        ellipse(400, 400, 15);
+    }
+
+    for (let i=0; i<55; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/55)*i;
+        translate(
+            sin(angle)*185,
+            cos(angle)*185
+        );
+        ellipse(400, 400, 16);
+    }
+
 
 
 }
