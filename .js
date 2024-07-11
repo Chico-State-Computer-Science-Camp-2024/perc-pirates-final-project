eba1ad25a5c5f64121994829d6ae52d99d45e3f3
k3llucky <kjrobinson@csuchico.edu>
2024-07-11 06:52:28 +0000

    made

---
+++ .js
@@ -0,0 +1,178 @@
+function setup(){
+    createCanvas (1000, 1000);
+
+}
+
+
+ function draw() {
+    
+    background(20);
+    rotate(ellipse)
+
+    for (let i=0; i<6; i++) {
+        resetMatrix(); 
+        let angle = (TWO_PI/6)*i;
+        translate(
+            sin(angle)*1,
+            cos(angle)*1,
+        );
+        ellipse(500, 500, 2);
+    }
+    
+
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*550,
+                cos(angle)*550,
+            );
+            ellipse(500, 500, 80);
+        }
+
+        for (let i=0; i<10; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/8)*i;
+            translate(
+                sin(angle)*550,
+                cos(angle)*550,
+            );
+            
+            ellipse(500, 500, 79);
+        }
+
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*40,
+                cos(angle)*40,
+            );
+            
+            ellipse(500, 500, 13);
+        }
+
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*50,
+                cos(angle)*50,
+            );
+            
+            ellipse(500, 500, 14);
+        }
+
+
+for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*60,
+                cos(angle)*60,
+            );
+            
+            ellipse(500, 500, 15);
+        }
+
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*70,
+                cos(angle)*70,
+            );
+            
+            ellipse(500, 500, 16);
+        }
+
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*80,
+                cos(angle)*80,
+            );
+            
+            ellipse(500, 500, 17);
+        }
+
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*90,
+                cos(angle)*90,
+            );
+            
+            ellipse(500, 500, 18);
+        }
+
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*100,
+                cos(angle)*100,
+            );
+            
+            ellipse(500, 500, 19);
+        }
+
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*110,
+                cos(angle)*110,
+            );
+            
+            ellipse(500, 500, 20);
+        }
+
+
+       for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*120,
+                cos(angle)*120,
+            );
+            
+            ellipse(500, 500, 21);
+        }
+
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*130,
+                cos(angle)*130,
+            );
+            
+            ellipse(500, 500, 22);
+        }
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*140,
+                cos(angle)*140,
+            );
+            
+            ellipse(500, 500, 23);
+        }
+
+        for (let i=0; i<6; i++) {
+            resetMatrix(); 
+            let angle = (TWO_PI/6)*i;
+            translate(
+                sin(angle)*150,
+                cos(angle)*150,
+            );
+            
+            ellipse(500, 500, 24);
+        }
+
+
+}
